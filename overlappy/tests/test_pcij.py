3065a2a067bfc4b2a8057b47ff08f4378a3b7380
Will Barnes <will.t.barnes@gmail.com>
2023-07-24 11:38:57 -0400

    fix and add tests

--- overlappy/tests/test_pcij.py
+++ overlappy/tests/test_pcij.py
@@ -14,6 +14,11 @@ from overlappy.wcs import pcij_matrix
     (0*u.deg, 0*u.deg, 2, [[1, 0, -2], [0, 1, 0], [0, 0, 1]]),
     (0*u.deg, 0*u.deg, -1, [[1, 0, 1], [0, 1, 0], [0, 0, 1]]),
     (90*u.deg, 0*u.deg, 0, [[0, -1, 0], [1, 0, 0], [0, 0, 1]]),
+    (90*u.deg, 0*u.deg, 1, [[0, -1, 0], [1, 0, -1], [0, 0, 1]]),
+    (0*u.deg, 45*u.deg, 0, np.eye(3)),
+    (0*u.deg, 90*u.deg, 1, [[1, 0, 0], [0, 1, 1], [0, 0, 1]]),
+    (30*u.deg, 30*u.deg, 1, [[np.sqrt(3)/2, -1/2, -1], [1/2, np.sqrt(3)/2, 0], [0, 0, 1]]),
+    (60*u.deg, 30*u.deg, 3, [[1/2, -np.sqrt(3)/2, -3*np.sqrt(3)/2], [np.sqrt(3)/2, 1/2, -3/2], [0, 0, 1]])
 ])
 def test_pcij_simple(roll_angle, grating_angle, order, expected_matrix):
     pcij = pcij_matrix(roll_angle=roll_angle,
